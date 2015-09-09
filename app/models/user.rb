bcbb07a1e82ea1fcbdee9dc40d3266459757a663
Yuki Sako <yukisakoatoz@gmail.com>
2015-09-09 21:59:30 +0900

    add sign up and user detail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  #\Aで^と, \zで$と同じ意味 /iで大文字小文字無視
+  validates :email, presence:true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX} , uniqueness: {case_sensitive: false}
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root to: 'static_pages#home'
+  get 'signup', to: 'users#new'
+
+  resources :users
 end
