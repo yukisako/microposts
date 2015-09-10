aa62a70be3b942fa63fd610d8ccdf042f3164d35
Yuki Sako <yukisakoatoz@gmail.com>
2015-09-10 12:59:17 +0900

    create log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root to: 'static_pages#home'
   get 'signup', to: 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
