9b79acdad46709beab1ddab4d674366a3c791c77
Yuki Sako <yukisakoatoz@gmail.com>
2015-09-11 22:30:06 +0900

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence:true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX} , uniqueness: {case_sensitive: false}
   has_secure_password
   #年齢は任意入力、0歳以上100歳以下、数字とする
-  validates :age, presence:false,numericality: {only_integer:true, greater_than_or_equal_to: 0,less_than: 100}, if: :logged_in?
+  validates :age, presence:false,numericality: {only_integer:true, greater_than_or_equal_to: 0,less_than: 100}, on: :update
   #一言は入力任意、2文字以上100文字以内とする
-  validates :phrase, presence:false, length:{ maximum:100, minimum:2}, if: :logged_in?
+  validates :phrase, presence:false, length:{ maximum:100, minimum:0}, on: :update
 end
