d454d546a850d0524b0ed2ca88e5b4083436c9af
Yuki Sako <divine_wind_820@yahoo.co.jp>
2015-09-19 12:55:49 +0000

    big fixed

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,16 @@ class FavoritesController < ApplicationController
     @tweet = params[:favorite]
     @user = params[:user_id]
     current_user.favorite(@tweet)
-    redirect_to user_path(@user)
+    #redirect_to user_path(@user) #ホームでお気に入りに追加したときにuser画面に戻られたら困る
+    redirect_to :back
   end
 
   def destroy
     @tweet = params[:favorite]
     @user = params[:user_id]
     current_user.back_favorite(@tweet)
-    redirect_to user_path(@user)
+    #redirect_to user_path(@user) #ホームでお気に入りに追加したときにuser画面に戻られたら困る
+    redirect_to :back
   end
   
 end
